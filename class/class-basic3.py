cbe72fef6a26d32f445dfea3064899dca823640d
linth <zhua0404@gmail.com>
2019-03-19 01:34:38 +0800

    [Add] class inheritance.

---
+++ class/class-basic3.py
@@ -0,0 +1,39 @@
+
+
+# example 1: add function from x-y axis to x-y-z axis.
+class A:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show_print(self):
+        print('x-axis is {0}; y-axis is {1}'.format(self.x, self.y))
+
+    def get_value(self):
+        return "%s, %s" % (self.x, self.y)
+
+
+class B(A):
+    def __init__(self, x, y, z):
+        super(B, self).__init__(x, y)
+        self.z = z
+
+    def show_print(self):
+        # super(B, self).show_print()
+        print('x-axis is {0}; y-axis is {1}; z-axis is {2}'.format(self.x, self.y, self.z))
+
+    def get_value(self):
+        return "%s, %s, %s" % (self.x, self.y, self.z)
+
+if __name__ == '__main__':
+    a = A(1, 2)
+    a.show_print()
+    print("(" + a.get_value() + ")")
+
+    b = B(1, 2, 3)
+    b.show_print()
+    print("(" + b.get_value() + ")")
+
+    c = B(1, 3)
+    c.show_print()
+    print(c.get_value())
