8bd0400a68c46686b256da89884ddcdb79998684
linth <zhua0404@gmail.com>
2019-09-16 00:26:18 +0800

    [Add] design pattern for factory.

--- class/class-static-classmethod.py
+++ class/class-static-classmethod.py
@@ -24,18 +24,32 @@ class Student:
         return age > 18
 
 
+class Robot:
+    def introduce_self(self):
+        print('My name is ' + self.name)
+
+
+class YmvsStudent(Student):
+    def __init__(self, student_id):
+        super().__init__()
+        self.id = student_id
+
 if __name__ == '__main__':
-    ''' Case 1: you have to create the instance if you want to use the class. '''
-    s = Student('george', 15)
-    s.print_name()
-    g = s.from_birthday_year('george', 1984)
-    print('The age of george is {}-years-old'.format(g.age))
-
-    ''' Case 2: use class method. '''
-    s1 = Student.from_birthday_year('May', 20)
-    print(s1.age, s1.name)
-
-    ''' Case 3: use static method. '''
-    print(Student.is_adult(22))
+    # ''' Case 1: you have to create the instance if you want to use the class. '''
+    # s = Student('george', 15)
+    # s.print_name()
+    # g = s.from_birthday_year('george', 1984)
+    # print('The age of george is {}-years-old'.format(g.age))
+    #
+    # ''' Case 2: use class method. '''
+    # s1 = Student.from_birthday_year('May', 20)
+    # print(s1.age, s1.name)
+    #
+    # ''' Case 3: use static method. '''
+    # print(Student.is_adult(22))
+    s = Student('george', 20)
+    print(s.name, s.age)
+    y = YmvsStudent(1)
+    print(y.student_id)
 
 
---
+++ design_pattern/factory/factory-basic.py
@@ -0,0 +1,8 @@
+'''
+Reference:
+    - https://www.djangospin.com/design-patterns-python/
+    - https://www.djangospin.com/design-patterns-python/factory/
+    - https://refactoring.guru/design-patterns/factory-method
+    - https://github.com/cundi/Django-Design-Patterns-and-Best-Practices/blob/master/%E7%AC%AC%E4%B8%80%E7%AB%A0-Django%E4%B8%8E%E6%A8%A1%E5%BC%8F.md
+    - https://github.com/faif/python-patterns
+'''
