f9b669f567e8253a16782f2d8b6da2d0f4f39bf6
linth <zhua0404@gmail.com>
2019-12-20 00:30:27 +0800

    try-except example

---
+++ try-except/simple-try-except.py
@@ -0,0 +1,28 @@
+
+
+class Solution:
+
+    def input_two_numbers(self):
+        try:
+            x = int(input('Please type the first number.'))
+            y = int(input('Please type the second number.'))
+
+            res = self.division(x, y)
+            print('the result of res is ', res)
+        except ZeroDivisionError as e:
+            print('It\'s not suitable, Please re-try again.')
+            print(e)
+        except ValueError as e:
+            print('It\'s not a number, Please re-try again.')
+            print(e)
+        finally:
+            self.input_two_numbers()
+
+    @staticmethod
+    def division(a, b):
+        return a / b
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.input_two_numbers()
