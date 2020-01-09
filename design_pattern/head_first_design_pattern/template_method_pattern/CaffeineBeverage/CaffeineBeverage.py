91b55bb4d5caed9b2730e21a99dbead273e6b78b
george_lin <zhua0404@gmail.com>
2020-01-09 16:33:48 +0800

    add a example of template method pattern.

---
+++ design_pattern/head_first_design_pattern/template_method_pattern/CaffeineBeverage/CaffeineBeverage.py
@@ -0,0 +1,16 @@
+
+
+class CaffeineBeverage:
+    """  Abstract base classes. """
+
+    def __init__(self):
+        pass
+
+    def prepare_recipe(self):
+        pass
+
+    def boil_water(self):
+        print('boil_water')
+
+    def pour_in_cup(self):
+        print('pour_in_cup')
