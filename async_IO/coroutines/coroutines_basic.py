2e274d53c8317f5bfa426bc511b2df987737092d
george_lin <george_lin@goglobal.com.tw>
2020-07-31 13:52:15 +0800

    add a example for coroutines.

---
+++ async_IO/coroutines/coroutines_basic.py
@@ -0,0 +1,28 @@
+import asyncio
+
+
+async def show_hello():
+    print("Hello")
+    await asyncio.sleep(5)
+    print("world.")
+
+
+async def show_hi():
+    await asyncio.sleep(1)
+    print("Hi")
+    await asyncio.sleep(3)
+    print("everyone")
+
+
+if __name__ == '__main__':
+    # TODO: get_event_loop for python 3.7 before.
+    #  if using after python 3.7, you can use .run() function.
+    loop = asyncio.get_event_loop()
+    # loop.run_until_complete(show_hello())
+
+    # the process of task is show_hello() -> show_hi().
+    task = [
+        asyncio.ensure_future(show_hello()),
+        asyncio.ensure_future(show_hi()),
+    ]
+    loop.run_until_complete(asyncio.wait(task))
