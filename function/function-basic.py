8247736792311e236e8b0893a9099af83e1657b7
george_lin <george_lin@goglobal.com.tw>
2020-08-03 16:06:59 +0800

    add the type of argument in the function.

--- function/function-basic.py
+++ function/function-basic.py
@@ -1,6 +1,6 @@
-'''
-    function
-'''
+"""
+function
+"""
 
 
 def function():
@@ -16,6 +16,20 @@ def add(*args):
     for i in args:
         sum += i
     return sum
+
+
+def greeting(name: str) -> str:
+    return 'Hello ' + name
+
+
+def new_add(x: int, y: int) -> int:
+    """
+    sum of two int numbers. please reference the function of add and compare with them.
+    :param x:
+    :param y:
+    :return:
+    """
+    return x+y
 
 
 def show_kwargs(**kwargs):
@@ -55,3 +69,9 @@ if __name__ == '__main__':
     # args (6, 3, 4, 1) => tuple
     # kwargs {'name': 'George', 'id': '888', 'weight': 66, 'height': 180}
     example1(1, 2, 6, 3, 4, 1, name='George', id='888', weight=66, height=180)
+
+    # the type of argument will be checked first.
+    res1 = greeting('george')
+    res2 = new_add(11, 1)
+    print(res1)
+    print(res2)
