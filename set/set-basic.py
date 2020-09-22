99726b57dc0e9df5977bf0a098f4c596a1733636
linth <zhua0404@gmail.com>
2020-09-22 20:43:48 +0800

    modify example of set.

--- set/set-basic.py
+++ set/set-basic.py
@@ -8,33 +8,58 @@ def example():
     print("banana" in s) # True
 
 
-def add_set_example():
-    """ Once a set is created, you cannot change its items, but you can add new items. """
+def example2():
+    t3 = set(("apple", "banana", "cherry"))
+    print(t3)
+
+
+def add_item():
+    """
+    add function.
+    Once a set is created, you cannot change its items, but you can add new items.
+    :return:
+    """
     s = {"apple", "banana", "cherry"}
     s.add("orange")
     print('The set = {}'.format(s))
     print('The length of set is {}'.format(len(s)))
 
 
-def remove_the_set_example():
+def remove_item():
+    """
+    remove function.
+    :return:
+    """
     s = {"apple", "banana", "cherry"}
     s.remove("banana") # If the item to remove does not exist, remove() will raise an error.
-    print('After deleting the banana, the set becomes {}'.format(s)) # {'cherry', 'apple', 'orange'}
+    print('After deleting the banana, the set becomes {}'.format(s)) # {'cherry', 'apple'}
 
 
-def discard_the_set_example():
+def discard_item():
+    """
+    discard function.
+    :return:
+    """
     s = {"apple", "banana", "cherry"}
     s.discard("banana") # If the item to remove does not exist, discard() will NOT raise an error.
-    print('After discarding the banana, the set becomes {}'.format(s)) # {'cherry', 'apple', 'orange'}
+    print('After discarding the banana, the set becomes {}'.format(s)) # {'cherry', 'apple'}
 
 
-def pop_the_set_example():
+def pop_item():
+    """
+    pop function.
+    :return:
+    """
     s = {"apple", "banana", "cherry"}
     s.pop() # remove the lastest one.
     print('Deleting the lastest one in the set, and the set is {}'.format(s)) # {'apple', 'orange'}
 
 
-def clear_the_set_example():
+def clear_item():
+    """
+    clear function.
+    :return:
+    """
     s = set()
     s.add('apple')
     s.add('banana')
@@ -43,7 +68,11 @@ def clear_the_set_example():
     print('Clearing the set, and the set is {}'.format(s))
 
 
-def del_the_set_example():
+def del_item():
+    """
+    delete function.
+    :return:
+    """
     s = set()
     s.add('apple')
     s.add('banana')
@@ -52,21 +81,16 @@ def del_the_set_example():
     # print(set) # UnboundLocalError: local variable 'set' referenced before assignment
 
 
-def example2():
-    t3 = set(("apple", "banana", "cherry"))
-    print(t3)
-
-
 def main():
     example()
     example2()
 
-    add_set_example()
-    remove_the_set_example()
-    discard_the_set_example()
-    pop_the_set_example()
-    clear_the_set_example()
-    del_the_set_example()
+    add_item()
+    remove_item()
+    discard_item()
+    pop_item()
+    clear_item()
+    del_item()
 
 
 if __name__ == '__main__':
