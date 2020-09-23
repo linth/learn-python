18881c28299855f1172f7fcf2c17f530707810ad
linth <zhua0404@gmail.com>
2020-09-24 01:18:06 +0800

    modify the example of try-catch/

--- try-except/simple-try-except.py
+++ try-except/simple-try-except.py
@@ -2,44 +2,64 @@
 References:
     - https://realpython.com/python-exceptions/
 """
+error_code = {
+    'general_error': 100,
+    'zero division error': 101,
+    'value error': 102,
+}
 
 
-class Solution:
+class CustomException(Exception):
+    def __init__(self, error_msg, code):
+        self._error_msg = error_msg
+        self._status_code = code
 
+
+class DivisionGame:
+    """ This is a division game. """
     def __init__(self):
         self.x = None
         self.y = None
         self.num_of_closing_app = 3
 
-    def input_two_numbers(self):
-        self.close_app()
+    def division_two_numbers(self):
         try:
-            x = int(input('Please type the first number.'))
-            y = int(input('Please type the second number.'))
+            x = int(input('Please type the first number.\n'))
+            y = int(input('Please type the second number.\n'))
 
             res = self.division(x, y)
-            print('the result of res is ', res)
+            print(f'The result of res is {res}')
+            print(f'Num of closing app: {self.num_of_closing_app}')
         except ZeroDivisionError as e:
-            print('It\'s not suitable, Please re-try again.')
-            print(e)
+            print(f'It\'s not suitable, please try again.')
+            self.close_app()
+            # raise CustomException(e, error_code['zero division error'])
         except ValueError as e:
-            print('It\'s not a number, Please re-try again.')
-            print(e)
+            print(f'It\'s not an integer number, please try again.')
+            self.close_app()
+            # raise CustomException(e, error_code['value error'])
+        except Exception as e:
+            print(f'Something wrong, please try again.')
+            self.close_app()
+            # raise CustomException(e, error_code['general_error'])
         finally:
             if self.num_of_closing_app > 0:
-                self.input_two_numbers()
+                print(f'You have {self.num_of_closing_app} time to execute the game.')
+                self.division_two_numbers()
+            else:
+                print(f'Game Over! Thank you.')
 
     def close_app(self):
         self.num_of_closing_app -= 1
 
     @staticmethod
-    def division(a, b):
+    def division(a: int, b: int):
         return a / b
 
 
 if __name__ == '__main__':
-    s = Solution()
-    s.input_two_numbers()
+    s = DivisionGame()
+    s.division_two_numbers()
     # s.input_two_numbers()
 
     # a = [1, 2, 3, 1, 2, 3, 4, 1]
