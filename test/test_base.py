bd46c8aea24d531d0bd717d49e294a308666024e
linth <zhua0404@gmail.com>
2021-01-03 23:15:21 +0800

    add test simple file.

--- test/test_base.py
+++ test/test_base.py
@@ -1,6 +1,7 @@
-'''
+"""
 References:
     - https://realpython.com/testing-in-django-part-1-best-practices-and-examples/
-'''
+    - https://imsardine.wordpress.com/tech/unit-testing-in-python/
+"""
 
 
