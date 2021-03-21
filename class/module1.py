1fa3c17f5743fd18f57b93fb069cd0cc8c63787e
linth <zhua0404@gmail.com>
2021-03-22 01:28:36 +0800

    add module example.

--- class/module1.py
+++ class/module1.py
@@ -1,6 +1,12 @@
-'''
+"""
 References:
     - https://www.brilliantcode.net/726/python3-6-module-io/
-'''
+"""
+
+import moduleConsole
+# from moduleConsole import add
 
 
+if __name__ == '__main__':
+    print('the result of add function: ', moduleConsole.add(3))
+    print('the result of product function: ', moduleConsole.product(5))
