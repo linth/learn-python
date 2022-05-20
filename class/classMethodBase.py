8e9246f0c7b447d27b619b10800b86935124e141
linth <zhua0404@gmail.com>
2022-05-21 01:08:06 +0800

    add property and class method.

--- class/classMethodBase.py
+++ class/classMethodBase.py
@@ -50,4 +50,26 @@ A: use helper function.
 """
 
 
+def generate_input(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+
+def execute(workers):
+    thread = [Thread(target=w.map) for w in workers]
+
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, rest = workers[0], workers[1:]
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
 
