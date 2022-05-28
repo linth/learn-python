3ac826c8b6c626016909bc41f351b4143b0c2988
linth <zhua0404@gmail.com>
2022-05-28 22:04:45 +0800

    add execute time with using decorator.

---
+++ decorator/base/cal_execute_time.py
@@ -0,0 +1,26 @@
+"""
+思考可以使用裝飾器來提供函式的執行所需時間？
+    - 單純裝飾器
+    - 使用裝飾器工廠
+"""
+import time
+
+def spend_how_much_time(callback):
+    def run():
+        start_time = time.time()
+        callback()
+        end_time = time.time()
+        print(f'total time: ', end_time - start_time)
+    return run
+
+@spend_how_much_time
+def function1():
+    sum = 0
+    for i in range(1000000001):
+        sum += i
+    print(f'sum: ', sum)
+
+
+function1()
+
+# todo: using decorator factory.
