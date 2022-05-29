1f4706a70adbd4096dec7d60d80e62d0a19aa205
linth <zhua0404@gmail.com>
2022-05-29 20:13:38 +0800

    add ternary conditional operator example.

---
+++ conditions/ternary_conditional_operator.py
@@ -0,0 +1,15 @@
+"""
+三元運算子(ternary conditional operator)
+
+
+Reference:
+    - https://jianjiesun.medium.com/python%E7%AD%86%E8%A8%98-6-%E4%B8%89%E5%85%83%E9%81%8B%E7%AE%97%E5%AD%90-ternary-conditional-operator-72e200575e49
+"""
+
+if __name__ == '__main__':
+    
+    a = 1
+    b = 2
+    
+    result = a if a > b else b
+    print(result)
