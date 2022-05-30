0a9adbf5b9c893c2093e1908cf6836a73ecb863e
linth <zhua0404@gmail.com>
2022-05-30 21:48:06 +0800

    modify comments in the example.

--- test/test_base.py
+++ test/test_base.py
@@ -1,10 +1,9 @@
 """
+Test example.
+
 References:
     - https://realpython.com/testing-in-django-part-1-best-practices-and-examples/
-<<<<<<< HEAD
     - https://imsardine.wordpress.com/tech/unit-testing-in-python/
-=======
->>>>>>> 61172c689804745c605d3424aa8c71ab7de8b382
 """
 
 
