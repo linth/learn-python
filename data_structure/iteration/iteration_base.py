735de4bf7e4e5a98c7701d2dec5205f3beca6a1c
linth <zhua0404@gmail.com>
2022-05-31 13:44:27 +0800

    add basic iteration and recursive comments.

---
+++ data_structure/iteration/iteration_base.py
@@ -0,0 +1,19 @@
+"""
+Iterative (疊代法)
+    - 是把已求得的數值，不斷重覆代入，再求得新數值的方法。
+    
+0! = 1
+1! = 0!*1=1
+2! = 1!*2 = 2
+3! = 2!*3 = 6
+...
+n! = (n-1)! * n
+
+比較：
+    - 疊代法(iterative method)是用迴圈去循環重複程式碼的某些部分來得到答案
+    - 遞迴法(recursive method)則是重複呼叫自身程式碼來得到答案。
+
+Reference:
+    - http://simonsays-tw.com/web/Recursion/Iteration&Recursion.html
+"""
+
