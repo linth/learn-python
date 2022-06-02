6e4bc31c5a97391add7b6ef1d49beafb04a79851
linth <zhua0404@gmail.com>
2022-06-02 21:06:14 +0800

    add sync example.

---
+++ async_IO/coroutines/ex_sync_async/sync_execute.py
@@ -0,0 +1,22 @@
+'''
+
+Reference:
+    - https://juejin.cn/post/7103036642180644871
+'''
+def funOne():
+    return ("function one.")
+
+def funTwo():
+    return ("function two.")
+
+def funThree():
+    return ("function three.")
+
+def run():
+    print(funOne())
+    print(funTwo())
+    print(funThree())
+
+if __name__ == '__main__':
+    run()
+    
