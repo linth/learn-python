b6ca3c4436315d7b13d563590839900110fac181
linth <zhua0404@gmail.com>
2022-06-09 17:16:34 +0800

    add async: create_task() example.

---
+++ async_IO/coroutines/ex/create_several_tasks.py
@@ -0,0 +1,34 @@
+'''
+使用 create_task() 來建立多個小任務
+
+Reference:
+    - https://docs.python.org/zh-tw/3/library/asyncio-task.html
+'''
+import asyncio
+import time
+
+
+async def print_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def main():
+    
+    task1 = asyncio.create_task(
+        print_after(1, 'hello')
+    )
+    
+    task2 = asyncio.create_task(
+        print_after(2, 'world')
+    )
+    
+    
+    print(f"started at {time.strftime('%X')}")
+    await task1
+    await task2
+    print(f"finished at {time.strftime('%X')}")
+    
+    
+# 注意，预期的输出顯示程式碼的運行時間比之前快了 1 秒: (可能使用此方式會加快速度!!!)
+asyncio.run(main())
