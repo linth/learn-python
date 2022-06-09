49b47ac75a577ffaeaede249b78b4ce4552c82f3
linth <zhua0404@gmail.com>
2022-06-09 17:04:25 +0800

    add print hello world example by async.

---
+++ async_IO/coroutines/ex/print_hello_world.py
@@ -0,0 +1,17 @@
+'''
+列印出 hello 後，隔一秒再列印出 world.
+
+Reference:
+    - https://docs.python.org/zh-tw/3/library/asyncio-task.html
+'''
+import asyncio
+import time
+
+
+async def main():
+    print('hello')
+    await asyncio.sleep(1)    
+    print('world')
+    
+
+asyncio.run(main())
