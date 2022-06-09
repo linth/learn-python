2180442885fbc9af1c1de8f4688817680da0743e
linth <zhua0404@gmail.com>
2022-06-09 17:16:20 +0800

    add async example.

---
+++ async_IO/coroutines/ex/print_two_words_with_delay.py
@@ -0,0 +1,27 @@
+'''
+隔幾秒後，列印出 hello 後，隔幾秒再列印出 world.
+
+Reference:
+    - https://docs.python.org/zh-tw/3/library/asyncio-task.html
+
+'''
+import asyncio
+import time
+
+
+async def print_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+    
+    
+async def main():
+    # 按照順序執行
+    
+    print(f"started at {time.strftime('%X')}")
+    
+    await print_after(1, 'hello')
+    await print_after(2, 'world')
+    
+    print(f"finished at {time.strftime('%X')}")
+    
+asyncio.run(main())
