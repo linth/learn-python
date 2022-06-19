24a213eae6041115402078a26488115f8a95bdce
linth <zhua0404@gmail.com>
2022-06-19 19:52:28 +0800

    add collections counters.

---
+++ data_structure/collections/base/collection_counters.py
@@ -0,0 +1,12 @@
+'''
+Counters in collection.
+
+
+Reference:
+    - https://www.geeksforgeeks.org/python-collections-module/
+'''
+from collections import Counter 
+
+l = ['B','B','A','B','C','A','B','B','A','C']
+
+print(Counter(l))
