a6e058d633b12672b7f770ed5cf3977c906cf95f
linth <zhua0404@gmail.com>
2022-07-08 17:59:39 +0800

    add comments.

--- async_IO/multithreading/base/6_lock_threads.py
+++ async_IO/multithreading/base/6_lock_threads.py
@@ -1,7 +1,10 @@
 '''
 學習如何使用 lock + threads
+    - 在平行化的多執行緒程式中，每個執行緒都是同時在執行的，若遇到不可以讓多個執行緒同時進行的工作時（例如將資料寫入同一個檔案），就必須使用鎖定（lock）的方式，一次只讓一個執行緒處理這種工作。
 
-
+    - 當一個執行緒呼叫了 Lock 的 acquire 時，代表取得了這個 Lock 的使用權，接著它就可以往下執行裡面的工作，若此時又有另外一個執行緒想要呼叫 acquire 取得使用權的話，就必須等待上一個執行緒執行完，並呼叫 release 釋放這個 Lock 之後，才能夠取得這個 Lock 的使用權，接著執行裡面的工作。
+    - 在這種狀況下雖然兩個 Worker 是同時執行的，但是由於 Lock 的互斥作用，因此可以確保被 Lock 的 acquire 與 release 包起來的這段程式碼不會被兩個執行緒同時執行。
+    
 Reference:
     - https://blog.gtwang.org/programming/python-threading-multithreaded-programming-tutorial/
 '''
