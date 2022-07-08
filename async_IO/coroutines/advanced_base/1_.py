54a1e822f1f2f8ca5fb1b1a14c1a9b8e8230bfa9
linth <zhua0404@gmail.com>
2022-07-08 20:49:29 +0800

    增加一個初步範例

---
+++ async_IO/coroutines/advanced_base/1_.py
@@ -0,0 +1,13 @@
+'''
+使用 multiple-processing, multiple-threading 範例
+    - 建立需比較共享資源的範例
+
+
+參考兩個範例檔案: 6_not_consider_critical_section.py, 7_consider_critical_section.py 
+結合考慮 thread, process
+
+
+Reference:
+    - https://medium.com/velotio-perspectives/an-introduction-to-asynchronous-programming-in-python-af0189a88bbb
+'''
+
