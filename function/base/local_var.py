c0e9e2d48307810ac09bcb85d3f0258f88287730
linth <zhua0404@gmail.com>
2022-07-09 22:35:16 +0800

    update function example.

--- function/base/local_var.py
+++ function/base/local_var.py
@@ -2,20 +2,23 @@
 函式(Function)變數範圍(Scope)
     - 區域變數(Local Variable)
     - 只有在函式內才可以進行存取
+    - 一般來說，全域變數是無法被該function scope內重新定義的變數進行存取。
     
     
 Reference:
     - https://www.learncodewithmike.com/2019/12/python-function.html
 '''
 
+x = 1000
+
+
 def cal_number():
-    # local variable.
+    # local variable. x = 100 無法對 cal_number() 外的 x 重新賦值。
     x = 100
     
     
 if __name__ == '__main__':
     
-    # print(x) # NameError: name 'x' is not defined
+    print(x) # 1000
     
-    cal_number()
     
