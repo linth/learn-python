42f8cbe1088ca3bc7cf1a28233c3b8dcb19339f9
linth <zhua0404@gmail.com>
2022-07-12 18:31:47 +0800

    move test example and delete test folder.

--- modules/unittest/test_django_base.py
+++ modules/unittest/test_django_base.py
@@ -1,9 +1,8 @@
 """
-Test example.
+Test in django example.
 
 References:
     - https://realpython.com/testing-in-django-part-1-best-practices-and-examples/
-    - https://imsardine.wordpress.com/tech/unit-testing-in-python/
 """
 
 
