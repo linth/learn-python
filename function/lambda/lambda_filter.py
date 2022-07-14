8f069bd9a4554178b50642ad460c19d7464fc88e
linth <zhua0404@gmail.com>
2022-07-14 16:22:12 +0800

    update filter example.

--- function/lambda/lambda_filter.py
+++ function/lambda/lambda_filter.py
@@ -7,10 +7,20 @@ filter程式撰寫方式：
     
 Reference:
     - https://www.learncodewithmike.com/2019/12/python-lambda-functions.html
+    - https://juejin.cn/post/6988877471500206093
 '''
 
 number = [50, 2, 12, 30, 27, 4]
 
 res = filter(lambda x : x > 10, number)
 print(list(res)) # [50, 12, 30, 27]
+
+
+number2 = [1, -2, -1, 2, 3, 4, -3]
+
+def is_positive(num):
+    return num > 0
+
+res2 = filter(is_positive, number2)
+print(list(res2))
 
