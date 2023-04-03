8b70530f2a07f6d652ddf7e40758dc0c11326a54
linth <zhua0404@gmail.com>
2023-04-04 01:03:58 +0800

    add serialization example.

---
+++ modules/serialization/pickle_ex.py
@@ -0,0 +1,17 @@
+'''
+Serialization 
+
+序列化:
+    - object -> bytes
+        - dumps()
+    - object -> file-like object
+        - dump()
+反序列化:
+    - bytes -> object
+        - loads()
+    - file-like object -> object
+        - load()
+        
+Reference:
+  - https://learnpython.com/blog/object-serialization-in-python/
+'''
