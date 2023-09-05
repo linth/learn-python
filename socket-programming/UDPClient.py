6a4efe7d31d1287cfa7ef6244ddbd5a8732d65ce
linth <zhua0404@gmail.com>
2023-09-06 00:53:56 +0800

    add socket programming.

---
+++ socket-programming/UDPClient.py
@@ -0,0 +1,18 @@
+'''
+UDP client
+
+'''
+# from socket import *
+
+
+# serverName = 'hostname'
+# serverPort = 12000
+# clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# # message = raw_input('Input lowercase sentence:')
+# message = 'Input lowercase sentence:'
+# clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+# print(modifiedMessage.decode())
+# clientSocket.close()
---
+++ socket-programming/UDPServer.py
@@ -0,0 +1,15 @@
+'''
+UDP server
+
+'''
+# from socket import *
+
+# serverPort = 12000
+# serverSocket = socket(AF_INET, SOCK_DGRAM)
+# serverSocket.bind(('', serverPort))
+# print('the server is ready to receive')
+
+# while True:
+#   message, clientAddress = serverSocket.recvfrom(2048)
+#   modifiedMessage = message.decode().upper()
+#   serverSocket.sendto(modifiedMessage.encode(), clientAddress)
