a77036ef3fd8a793dbe2e552ad3f9a66924ab034
linth <zhua0404@gmail.com>
2023-10-03 23:13:02 +0800

    add comments.

---
+++ SOLID/DIP/ex/payment.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+class IPayment(ABC):
+  
+    @abstractmethod
+    def pay(self, amount: int) -> None:
+        pass
+      
+      
+class PaymentProcessor:
+  
+    def __init__(self, iPayment: IPayment) -> None:
+        self.iPayment = iPayment
+
+    def execute_payment(self, amount: int) -> object:
+        ''' execute the process of payment. '''
+        self.iPayment.pay(amount)
+
+
+class CreditCardPayment(IPayment):
+    ''' using credit card to pay money. '''
+    def pay(self, amount: int) -> None:
+        print(f'paid ${amount} via credit card.')
+        return super().pay(amount)
+      
+
+class PayPalPayment(IPayment):
+    ''' using PayPal to pay money. '''
+    def pay(self, amount: int) -> None:
+        print(f'paid ${amount} via PayPal.')
+        return super().pay(amount)
+      
+      
+if __name__ == '__main__':
+    ccp = CreditCardPayment()
+    pp = PaymentProcessor(ccp)
+    pp.execute_payment(100)
